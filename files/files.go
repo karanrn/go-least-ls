2e80d49fa62df45567a86596e631176327cc7b3d
karanrn <karan4080@gmail.com>
2019-10-03 23:58:35 +0530

    Updated help usage.

--- files/files.go
+++ files/files.go
@@ -1,13 +1,13 @@
 package main
 
-import(
+import (
+	"flag"
 	"fmt"
+	"github.com/karanrn/go-least-ls/helper"
 	"os"
 	"path/filepath"
-	"time"
-	"flag"
 	"strconv"
-	"github.com/karanrn/go-least-ls/helper"
+	"time"
 )
 
 /*
@@ -17,24 +17,24 @@ import(
 
 // Walk the files in the path
 func visit(files *[]string) filepath.WalkFunc {
-    return func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            fmt.Println(err)
-        }
-        *files = append(*files, path)
-        return nil
-    }
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err)
+		}
+		*files = append(*files, path)
+		return nil
+	}
 }
 
 // Check the extension of the file
 // naked return : returns zero values when no match
 func Find(slice []string, val string) (i int, flag bool) {
-    for i, item := range slice {
-        if item == val {
-            return i, true
-        }
-    }
-    return
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return
 }
 
 // Flag usage
@@ -50,12 +50,12 @@ go-least-ls.exe -older 30 -count 10
 -help  : Gets the help.
 -older : How much older files? Accepts integer in units of days.
 -count : Number of files to view.`
-	
+
 	fmt.Printf(helpUsage)
 	os.Exit(0)
 }
 
-func main(){
+func main() {
 	var files []string
 	var lastFiles = make(map[string]time.Time)
 	var days int
@@ -65,15 +65,21 @@ func main(){
 	flag.Usage = toolUsage
 	older := flag.String("older", "", "how older?")
 	counter := flag.String("count", "", "number of files to view.")
+	help := flag.String("help", "", "Get usage help.")
 
 	flag.Parse()
 
 	if flag.NFlag() == 0 {
-		toolUsage()
-		os.Exit(-1)
+		fmt.Println("Check usage- go-least-ls.exe -help")
+		os.Exit(0)
 	}
 
-	if *older != "" && *counter != ""{
+	if *help == "" {
+		toolUsage()
+		os.Exit(0)
+	}
+
+	if *older != "" && *counter != "" {
 		var err error
 		days, err = strconv.Atoi(*older)
 		if err != nil {
@@ -87,8 +93,8 @@ func main(){
 			os.Exit(-1)
 		}
 
-	}else {
-		toolUsage()
+	} else {
+		fmt.Println("Check usage- go-least-ls.exe -help")
 	}
 
 	// 30 days before current time
@@ -100,7 +106,7 @@ func main(){
 	// Current folder/directory
 	root, err := os.Getwd()
 	err = filepath.Walk(root, visit(&files))
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
@@ -110,16 +116,16 @@ func main(){
 			panic(err)
 		}
 		// _, found := Find(documents, filepath.Ext(file))
-		
+
 		lastAccess := info.ModTime()
 		if lastAccess.Before(archiveDate) || lastAccess.Equal(archiveDate) {
-				//fmt.Printf("%s : %s\n",file, info.ModTime())
-				lastFiles[file] = lastAccess
+			//fmt.Printf("%s : %s\n",file, info.ModTime())
+			lastFiles[file] = lastAccess
 		}
 	}
 
 	sortedfiles := helper.Sort(lastFiles)
-	for i := 0; i < len(sortedfiles) ; i++ {
+	for i := 0; i < len(sortedfiles); i++ {
 		if count == 0 {
 			break
 		}
@@ -127,5 +133,5 @@ func main(){
 		fmt.Printf("%s : %s \n", f.Key, f.Value)
 		count = count - 1
 	}
-	
+
 }
